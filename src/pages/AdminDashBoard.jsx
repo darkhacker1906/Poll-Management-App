774f3074f0423760a7d7ecd1c10517aa2dd9fe37
darkhacker1906 <singhabhinandan634@gmail.com>
2024-02-14 12:33:39 +0530

    Added the login and signup authentication

--- src/pages/AdminDashBoard.jsx
+++ src/pages/AdminDashBoard.jsx
@@ -1,11 +1,10 @@
-import { Typography } from '@mui/material'
 import React from 'react'
 
 function AdminDashBoard() {
   return (
-<>
-<Typography color={"black"} variant='h2'>Admin DashBoard</Typography>
-</>
+    <div>
+      Admin
+    </div>
   )
 }
 
