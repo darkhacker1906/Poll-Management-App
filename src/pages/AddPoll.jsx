8969a052565037ff43d9412307b84f83b235dd20
darkhacker1906 <singhabhinandan634@gmail.com>
2024-02-20 14:14:11 +0530

    Check

--- src/pages/AddPoll.jsx
+++ src/pages/AddPoll.jsx
@@ -80,7 +80,7 @@ function AddPoll() {
               onSubmit={handleSubmit}
             >
               <Typography sx={{ textAlign: "center" }} variant="h4">
-                Add Poll
+                Add Poll This is for check
               </Typography>
               <TextField
                 variant="outlined"
